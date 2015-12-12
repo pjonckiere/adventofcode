b2988989d16a38cece7be290271d2bd9f2630d9a
pjonckiere <pieter.jonckiere@xio.be>
2015-12-12 14:06:25 +0100

    solution day6

---
+++ day6/1.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import re
+
+f = open('input.txt')
+
+grid = [[0]*1000 for i in range(1000)]
+for line in f:
+	# print line
+	regex = re.compile(r'^((\w| )[^\d]+)((\d|,)+) through ((\d|,)+)$')
+	matches = regex.match(line)
+	action = matches.group(1).strip()
+	coord1 =  matches.group(3)
+	x1 = int(coord1[:coord1.find(',')])
+	y1 = int(coord1[coord1.find(',') + 1:])
+	coord2 =  matches.group(5)
+	x2 = int(coord2[:coord2.find(',')])
+	y2 = int(coord2[coord2.find(',') + 1:])
+
+	for i in range(x1, x2 + 1):
+		for j in range(y1, y2 + 1):
+			if action == 'turn on':
+				grid[i][j] = 1
+			elif action == 'turn off':
+				grid[i][j] = 0
+			else:
+				toggle = [1, 0]
+				grid[i][j] = toggle[grid[i][j]]
+
+print sum(x.count(1) for x in grid)
